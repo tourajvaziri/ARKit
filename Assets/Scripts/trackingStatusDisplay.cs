540baf0e86d463726352490201e2ba70b735c969
Touraj Vaziri <vtouraj@gmail.com>
2018-04-16 19:46:05 -0700

    Adding exisiting files.

---
+++ Assets/Scripts/trackingStatusDisplay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.iOS;
+
+[RequireComponent(typeof(Text))]
+public class trackingStatusDisplay : MonoBehaviour {
+
+    private string _trackingState;
+    private Text _text;
+
+	// Use this for initialization
+	void Start () {
+        UnityARSessionNativeInterface.ARFrameUpdatedEvent += ARFrameUpdated;
+        _text = gameObject.GetComponent<Text>();
+	}
+
+    void ARFrameUpdated(UnityARCamera unityArCamera)
+    {
+        _trackingState = string.Format("TrackingState: {0}\n TrackingReason: {1}", unityArCamera.trackingState, unityArCamera.trackingReason);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        _text.text = _trackingState;
+	}
+}
---
+++ Assets/newScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+      
+        Debug.Log("SALALM");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
